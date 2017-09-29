7b902feed4c8f174c0cc899ad3c67cafb6e65259
Filip Ilic <filip.ilic@cern.ch>
2017-09-29 12:12:15 +0200

    Add FileTable class
    - handling the mapping between displayname / remote path
    - displaying table in TGListView
    - filling the table with urls that get transformed to key/val
    - filtering/querying

--- dqmPlotter.C
+++ dqmPlotter.C
@@ -1,62 +1,128 @@
-class TempClass {
+// ==================== START FILETABLE  ====================
+
+class FileTable {
 public:
-    TempClass(TGMainFrame*);
-
-    void PushResult(string t) {
-        file_box->AddEntry(t.c_str(), 0);
-    }
-
+    FileTable() {}
+    void FillFromFile(string path_to_file_to_load);
+    string GetPathFromName(string name);
+    string GetNameFromPath(string path);
+    FileTable FilterNamesBy(vector<string> filters);
+    void DisplayInListBox(TGListBox* listbox);
+    void PrintDebug();
+    void AddEntry(string k, string v);
 
 private:
-   TGVerticalFrame* top_frame;
-   TGListBox*       file_box;
-
+    map<string, string> table;          // key=name, value=path
 };
 
-TempClass::TempClass(TGMainFrame* main_frame) {
-    top_frame = new TGVerticalFrame(main_frame);
-    file_box  = new TGListBox(top_frame);
 
-    top_frame->AddFrame(file_box, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
-    main_frame->AddFrame(top_frame, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
+string FileTable::GetPathFromName(string name) {
+    return table[name];
 }
 
+void FileTable::FillFromFile(string path_to_file_to_load) {
+    cout << "Trying to load " << endl;
 
-class FileSearch {
+    std::ifstream in(path_to_file_to_load);
+
+    char str[255];
+
+    string filenamepath, displayname;
+    while(in) {
+        in.getline(str, 255);  // delim defaults to '\n'
+        filenamepath=string(str);
+
+        int beginIdx = filenamepath.rfind('/');
+        displayname = filenamepath.substr(beginIdx + 1);
+        this->AddEntry(displayname, filenamepath);
+    }
+}
+
+void FileTable::AddEntry(string k, string v) {
+    table[k] = v;
+}
+
+void FileTable::PrintDebug() {
+    for(auto& e : table)
+        cout << "key: " << e.first << "  value: " << e.second << endl;
+}
+
+void FileTable::DisplayInListBox(TGListBox* listbox) {
+    listbox->RemoveAll();
+    int i = 0;
+    for(auto& elem : table) {
+        listbox->AddEntry(elem.first.c_str(), i++);
+    }
+    listbox->Layout();
+}
+
+FileTable FileTable::FilterNamesBy(vector<string> filters) {
+    FileTable filtered;
+
+    for(auto& entry : table) {
+        bool is_ok = true;
+        for(auto& f : filters){
+
+            if(entry.first.find(f) != string::npos) {
+                is_ok &= true;
+            } else {
+                is_ok &= false;
+                break;
+            }
+        }
+        if(is_ok)
+            filtered.AddEntry(entry.first, entry.second);
+    }
+    return filtered;
+}
+
+// ==================== END FILETABLE  ====================
+
+
+
+
+
+// ==================== START BROWSER  ====================
+
+class Browser {
 
 public:
-    FileSearch(TGMainFrame*, TempClass *resclass);
+    Browser(TGMainFrame*);
     void GetItems() { cout << "FileSearch::GetItems" << endl; }
 
 
-    void ApplyFilter(); // @SLOT
-    void SelectFiles(); // @SLOT
+    void ApplyFilter();        // @SLOT
+    void SelectFiles();        // @SLOT
+    void ClearSelectedFiles(); // @SLOT
 
 private:
 
-    void LoadFilenames(string path);
-    void DisplayTable(vector<string>);
-
     TGVerticalFrame*   top_frame;
+    TGVerticalFrame*   bot_frame;
     TGHorizontalFrame* search_frame;
     TGTextEntry*       search_box;
-    TGListBox*         result_box;
+    TGListBox*         available_files_box;
     TGComboBox*        datamode_dropdown;
     TGComboBox*        module_dropdown;
     TGTextButton*      applyfilter_button;
     TGTextButton*      selectfiles_button;
     TList*             selection_in_box;
-    TempClass* tempclass;
 
-    vector<string>     table; // this contains all the paths to the remote files
+    TGListBox*         selected_files_box;
+    TGTextButton*      clear_selected_button;
+
+    FileTable          table;
 };
 
 
-FileSearch::FileSearch(TGMainFrame* main_frame, TempClass* resclass) {
+Browser::Browser(TGMainFrame* main_frame) {
+
+    table.FillFromFile("./rootfiles.txt");
+
     cout << "FileSearch::FileSearch" << endl;
 
-    tempclass = resclass;
     top_frame  = new TGVerticalFrame(main_frame);
+    bot_frame  = new TGVerticalFrame(main_frame);
 
     search_frame = new TGHorizontalFrame(top_frame);
     datamode_dropdown = new TGComboBox(search_frame, 100);
@@ -70,24 +136,31 @@ FileSearch::FileSearch(TGMainFrame* main_frame, TempClass* resclass) {
     search_frame->AddFrame(search_box, new TGLayoutHints(kLHintsExpandX));
     search_frame->AddFrame(applyfilter_button);
 
-    result_box = new TGListBox(top_frame);
+    available_files_box = new TGListBox(top_frame);
     selectfiles_button = new TGTextButton(top_frame, "Add selected File(s)");
 
+    selected_files_box = new TGListBox(bot_frame);
+    clear_selected_button = new TGTextButton(bot_frame, "Clear Selection");
+
     top_frame->AddFrame(search_frame, new TGLayoutHints(kLHintsExpandX, 2, 2, 2, 2));
-    top_frame->AddFrame(result_box, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
+    top_frame->AddFrame(available_files_box, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
     top_frame->AddFrame(selectfiles_button, new TGLayoutHints(kLHintsExpandX, 2, 2, 2, 2));
+
+    bot_frame->AddFrame(selected_files_box, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
+    bot_frame->AddFrame(clear_selected_button, new TGLayoutHints(kLHintsExpandX, 2, 2, 2, 2));
     main_frame->AddFrame(top_frame, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
+    main_frame->AddFrame(bot_frame, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 2, 2, 2, 2));
 
     datamode_dropdown->AddEntry("NOT IMPLEMENTED YET", 0);
     datamode_dropdown->AddEntry("Online", 1);
     datamode_dropdown->AddEntry("Offline", 2);
     datamode_dropdown->AddEntry("Relval", 3);
 
-    module_dropdown->AddEntry(".", 0);
+    module_dropdown->AddEntry("All", 0);
     module_dropdown->AddEntry("Ecal", 1);
     module_dropdown->AddEntry("Pixel", 2);
-    module_dropdown->AddEntry("PixelPhase1", 2);
-    module_dropdown->AddEntry("Strip", 3);
+    module_dropdown->AddEntry("PixelPhase1", 3);
+    module_dropdown->AddEntry("SiStrip", 4);
 
     datamode_dropdown->Resize(150, 20);
     module_dropdown->Resize(150, 20);
@@ -95,102 +168,68 @@ FileSearch::FileSearch(TGMainFrame* main_frame, TempClass* resclass) {
     datamode_dropdown->Select(0);
     module_dropdown->Select(0);
 
-    result_box->SetMultipleSelections(true);
+    available_files_box->SetMultipleSelections(true);
 
     applyfilter_button->SetToolTipText("Click here to print the selection you made");
-    applyfilter_button->Connect("Clicked()", "FileSearch", this, "ApplyFilter()");
+    applyfilter_button->Connect("Clicked()", "Browser", this, "ApplyFilter()");
 
-    selectfiles_button->Connect("Clicked()", "FileSearch", this, "SelectFiles()");
+    selectfiles_button->Connect("Clicked()", "Browser", this, "SelectFiles()");
+    clear_selected_button->Connect("Clicked()", "Browser", this, "ClearSelectedFiles()");
 
-    LoadFilenames("./rootfiles.txt");
-    DisplayTable(table);
-
+    table.DisplayInListBox(available_files_box);
 
     selection_in_box = new TList;
-
 }
 
-void FileSearch::ApplyFilter() {
-    cout << "applying filter" << endl;
-
-    result_box->RemoveAll();
-
+void Browser::ApplyFilter() {
+    available_files_box->RemoveAll();
     string module_str = module_dropdown->GetSelectedEntry()->GetTitle();
     string query_str = search_box->GetText();
 
-    vector<string> query_result;
+    vector<string> filters;
+    filters.push_back("_" + module_str + "_");
+    filters.push_back(query_str);
 
-    for(auto& elem : this->table) {
-        if ((elem.find("_" + module_str + "_") != string::npos) &&   // module filter
-            (elem.find(query_str) != string::npos)) {                // textbox filter
-            query_result.push_back(elem);
-        }
-    }
-
-    DisplayTable(query_result);
+    FileTable result = table.FilterNamesBy(filters);
+    result.DisplayInListBox(available_files_box);
 }
 
-void FileSearch::SelectFiles() {
-    cout << "selecting files" << endl;
+void Browser::ClearSelectedFiles() {
+    selected_files_box->RemoveAll();
+}
 
+void Browser::SelectFiles() {
     selection_in_box->Clear();
-    result_box->GetSelectedEntries(selection_in_box);
+    available_files_box->GetSelectedEntries(selection_in_box);
     selection_in_box->ls();
 
-    tempclass->PushResult("something");
-}
+    FileTable selected_files;
 
-void FileSearch::LoadFilenames(string path) {
-    cout << "FileSearch::LoadFilenames" << endl;
-    std::ifstream in(path);
-
-    char str[255];
-
-    int i = 0;
-    string tmp, displayname;
-    while(in) {
-        in.getline(str, 255);  // delim defaults to '\n'
-        tmp=string(str);
-
-        int beginIdx = tmp.rfind('/');
-        displayname = tmp.substr(beginIdx + 1);
-        table.push_back(displayname);
+    for(const auto&& obj: *selection_in_box) {
+        string obj_name = obj->GetTitle();
+        string val = table.GetPathFromName(obj_name);
+        cout << "found value " << val << endl;
+        selected_files.AddEntry(obj_name, val);
     }
+
+    selected_files.DisplayInListBox(selected_files_box);
+    selected_files.PrintDebug();
 }
 
-void FileSearch::DisplayTable(vector<string> t) {
-    result_box->RemoveAll();
-    int i = 0;
-    for(auto& elem : t) {
-        result_box->AddEntry(elem.c_str(), i++);
-    }
-    result_box->Layout();
-}
-
-
+// ==================== END BROWSER  ====================
 
 
 void dqmPlotter() {
     int width = 600;
     int height = 1000;
     TGMainFrame* main_frame = new TGMainFrame(gClient->GetRoot(), width, height);
-    TGMainFrame* sub_frame = new TGMainFrame(gClient->GetRoot(), width, height/2);
 
-
-    main_frame->SetWindowName("Search window");
-    sub_frame->SetWindowName("Selected files");
-
-
-    TempClass* tc = new TempClass(sub_frame);
-    FileSearch* fs = new FileSearch(main_frame, tc);
+    main_frame->SetWindowName("Browser");
+    Browser* fs = new Browser(main_frame);
 
     main_frame->MapSubwindows();
     main_frame->MapWindow();
 
     main_frame->MoveResize(100, 100, width, height);
 
-    sub_frame->MapSubwindows();
-    sub_frame->MapWindow();
-
-    sub_frame->MoveResize(100, 100, width, height/2);
 }
